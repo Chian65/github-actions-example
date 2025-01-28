6295e3ff8222cb8e95102ab76f378b1898ee6c52
Chian65 <93095352+Chian65@users.noreply.github.com>
2025-01-28 17:37:21 +0100

    Test

--- src/main/java/com/chian/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/chian/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome";
+		return "Welcome all";
 	}
 
 	public static void main(String[] args) {
